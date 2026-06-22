e7481b68411b0b042b5673b7c1fcb33df7316f2f
HARIBABU.D <148314058+HARIBABU1906@users.noreply.github.com>
2026-06-22 20:06:32 +0530

    Update movieApi.js

--- client/src/api/movieApi.js
+++ client/src/api/movieApi.js
@@ -2,12 +2,12 @@ import api from "./client";
 import { mockMovies } from "../mockData/movies";
 
 export async function login(payload) {
-  const response = await api.post("/auth/login", payload);
+  const response = await api.post("/api/auth/login", payload);
   return response.data;
 }
 
 export async function register(payload) {
-  const response = await api.post("/auth/register", payload);
+  const response = await api.post("/api/auth/register", payload);
   return response.data;
 }
 
