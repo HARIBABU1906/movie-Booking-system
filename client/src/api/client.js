010af1d3c0d4595f5302d4e8a8471bd3ba065598
HARIBABU.D <148314058+HARIBABU1906@users.noreply.github.com>
2026-06-22 19:29:28 +0530

    Update client.js

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL:"https://movie-booking-system-vhoq.onrender.com/",
+  baseURL:"https://movie-booking-system-vhoq.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
